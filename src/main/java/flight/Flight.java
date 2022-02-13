7e14bc3eb7c51d48db9b20f821c885c886e9a226
saimeerasingh <singhsaimeera@gmail.com>
2022-02-13 21:27:10 +0000

    flight class set up

--- src/main/java/flight/Flight.java
+++ src/main/java/flight/Flight.java
@@ -1,4 +1,35 @@
 package flight;
 
+import employee.CabinCrewMember;
+import employee.Pilot;
+import passenger.Passenger;
+import plane.Plane;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Flight {
+   private ArrayList <Pilot> pilots;
+   private ArrayList <CabinCrewMember> cabinCrewMembers;
+   private ArrayList <Passenger> passengers;
+   private Plane plane;
+   private String flightNumber;
+   private String destination;
+   private String departureAirport;
+   private String departureTime;
+
+        public Flight(ArrayList<Pilot> pilots, ArrayList<CabinCrewMember> cabinCrewMembers,
+                      Plane plane, String flightNumber,
+                      String destination, String departureAirport, String departureTime) {
+        this.pilots = pilots;
+        this.cabinCrewMembers = cabinCrewMembers;
+        this.passengers = new ArrayList<>();
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
+    
+
 }
